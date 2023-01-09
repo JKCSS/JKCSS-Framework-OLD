f79884b8c5579f1f006f63fad2de51cdb20d9ddc
JehanKandy <jehankandy@gmail.com>
2023-01-09 21:45:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,57 @@
 <link rel="stylesheet" href="css/site.css">
 <?php include("layouts/nav_bar.php"); ?>
 
+<div class="index-content">
+    <div class="box-container">
+        <img src="https://avatars.githubusercontent.com/u/111488170?s=200&v=4" alt="" class="index-img">
+        <div class="title">JKCSS - CSS Framework</div>
+        <hr>
+        <div class="body">
+            <div class="site-grid1">
+                <div class="item1">
+                    <div class="title">Components</div>
+                    <div class="body">
+                        <table border="0" class="tag-tbl">
+                            <tr>
+                                <td><a href="">Buttons</a></td>
+                                <td><a href="">Images</a></td>
+                                <td><a href="">Headers</a></td> 
+                                <td><a href="">Texts</a></td>                                    
+                            </tr>
+                            <tr>
+                                <td><a href="">Buttons</a></td>
+                                <td><a href="">Buttons</a></td>
+                                <td><a href="">Buttons</a></td> 
+                                <td><a href="">Buttons</a></td>                                    
+                            </tr>
+                            <tr>
+                                <td><a href="">Buttons</a></td>
+                                <td><a href="">Buttons</a></td>
+                                <td><a href="">Buttons</a></td> 
+                                <td><a href="">Buttons</a></td>                                    
+                            </tr>
+                            <tr>
+                                <td><a href="">Buttons</a></td>
+                                <td><a href="">Buttons</a></td>
+                                <td><a href="">Buttons</a></td> 
+                                <td><a href="">Buttons</a></td>                                    
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <div class="item2">
+                <div class="title">Include CDN</div>
+                    <div class="body">
+                        Ues CDN link in your webpage, to Get Start 
+                        click here
+
+                        <a href="intro.php"><button class="get-start-btn">Get Start</button></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <script src="js/script.js"></script>
