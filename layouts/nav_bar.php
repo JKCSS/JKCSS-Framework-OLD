dd15898c328f9b6ba02f56ffa85f6b1c33eeeddf
JehanKandy <jehankandy@gmail.com>
2023-01-09 15:11:42 +0530

    Update nav_bar.php

--- layouts/nav_bar.php
+++ layouts/nav_bar.php
@@ -1,3 +1,16 @@
 <link rel="stylesheet" href="../css/site.style.css">
 
+<nav class="nav-bar">
+    <div class="box-container">
+        <div class="title">JKCSS - CSS Framework</div>
+    
+        <div class="body">
+            <div class="version">
+                <span class="version-text">v1</span>
+                <span><button class="download-btn">Download</button></span>
+            </div>
+        </div>
+    </div>
+</nav>
+
 <script src="../js//script.js"></script>
