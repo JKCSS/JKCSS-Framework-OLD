65926722b576c7070bea45d4de5e3f4a9d21f525
JehanKandy <jehankandy@gmail.com>
2023-01-09 18:27:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <?php include("layouts/header.php"); ?>
-<link rel="stylesheet" href="css/site.style.css">
+<link rel="stylesheet" href="css/site.css">
 <?php include("layouts/nav_bar.php"); ?>
 
+<div class="home-content">
+    <div class="box-container">
+        <div class="title">Introduction</div>
+        <hr>
+        <div class="body">
+            This is basic CSS Framework that i developed. More versions are comming in future.
+        </div>
+        <div class="title">Documentations</div>
+        <hr>
+        <div class="body">
+            The Documentations of this CSS Framework, Can found in <a href="https://github.com/JKCSS/JKCSS-Framework" target="_blank">Github</a>
+
+            <div class="code-space">
+             <!DOCTYPE html>
+                <span style="color: blue;">&lt;!DOCTYPE html&gt;</span><br>
+                <span style="color: brown;">&lt;html <span style="color: red;">lang<span style="color: blue;">="en"</span></span>&gt</span><br>
+                
+
+            </div>
+        </div>
+    </div>
+</div>
 
 <script src="js/script.js"></script>
