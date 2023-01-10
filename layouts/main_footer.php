405881af384941157da4b4b6807cfd803381f953
JehanKandy <jehankandy@gmail.com>
2023-01-10 18:34:06 +0530

    Update main_footer.php

--- layouts/main_footer.php
+++ layouts/main_footer.php
@@ -23,55 +23,34 @@
                 <div class="title">
                     Links
                 </div>
-            </div>
-            <div class="fitem3">
-                <div class="title">
-                    Projects
-                </div>
-            </div>
-            <div class="fitem4">
-                <div class="title">
-                    Contact US
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<script src="../js//script.js"></script><link rel="stylesheet" href="../css/site.css">
-
-<div class="footer-content">
-    <div class="box-container">
-        <div class="footer-grid">
-            <div class="fitem1">
-                <div class="title">
-                    <span class="logo">
-                    <img src="https://avatars.githubusercontent.com/u/111488170?s=200&v=4" alt="" class="footer-logo"></span>
-                    <div class="title">
-                        JKCSS - CSS Framework
-                    </div>
-                    <div class="body">
-                        JKCSS is the CSS Framework. and you can use this Framework for design your website faster and responsive.
-                        <br></br>
-                        JKCSS, licensed <a href="https://github.com/JKCSS/JKCSS-Framework/blob/main/LICENSE" target="_blank" style="text-decoration: none; color:rgb(0, 88, 204); font-weight: 600;">MIT</a>
-                        <br><br><br>
-                        Currently v1
-                    </div>
-                </div>
-            </div>
-            <div class="fitem2">
-                <div class="title">
-                    Links
+                <div class="body">
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Docs</a></li>
+                        <li><a href="">Examples</a></li>
+                        <li><a href="">Templates</a></li>
+                        <li><a href="">Get Start</a></li>
+                    </ul>
                 </div>
             </div>
             <div class="fitem3">
                 <div class="title">
                     Projects
                 </div>
+                <div class="body">
+                    <ul>
+                        <li><a href="">JKCSS 1</a></li>
+                    </ul>
+                </div>
             </div>
             <div class="fitem4">
                 <div class="title">
                     Contact US
+                </div>
+                <div class="body">
+                    <ul>
+                        <li>+94 711758851</li>
+                    </ul>
                 </div>
             </div>
         </div>
