411a934398090e575d7ab0740631adfad07092d7
JehanKandy <jehankandy@gmail.com>
2023-01-10 07:00:10 +0530

    Add files via upload

---
+++ layouts/main_footer.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="../css/site.css">
+
+<div class="footer-content">
+    <div class="box-container">
+        <div class="footer-grid">
+            <div class="fitem1">item1</div>
+            <div class="fitem2">item2</div>
+            <div class="fitem3">item3</div>
+            <div class="fitem4">item4</div>
+        </div>
+    </div>
+</div>
+
+<script src="../js//script.js"></script>
