d8ca68cf609bc542c0bcb011c422a007a76a6299
JehanKandy <jehankandy@gmail.com>
2023-01-10 07:24:20 +0530

    Update main_footer.php

--- layouts/main_footer.php
+++ layouts/main_footer.php
@@ -3,10 +3,77 @@
 <div class="footer-content">
     <div class="box-container">
         <div class="footer-grid">
-            <div class="fitem1">item1</div>
-            <div class="fitem2">item2</div>
-            <div class="fitem3">item3</div>
-            <div class="fitem4">item4</div>
+            <div class="fitem1">
+                <div class="title">
+                    <span class="logo">
+                    <img src="https://avatars.githubusercontent.com/u/111488170?s=200&v=4" alt="" class="footer-logo"></span>
+                    <div class="title">
+                        JKCSS - CSS Framework
+                    </div>
+                    <div class="body">
+                        JKCSS is the CSS Framework. and you can use this Framework for design your website faster and responsive.
+                        <br></br>
+                        JKCSS, licensed <a href="https://github.com/JKCSS/JKCSS-Framework/blob/main/LICENSE" target="_blank" style="text-decoration: none; color:rgb(0, 88, 204); font-weight: 600;">MIT</a>
+                        <br><br><br>
+                        Currently v1
+                    </div>
+                </div>
+            </div>
+            <div class="fitem2">
+                <div class="title">
+                    Links
+                </div>
+            </div>
+            <div class="fitem3">
+                <div class="title">
+                    Projects
+                </div>
+            </div>
+            <div class="fitem4">
+                <div class="title">
+                    Contact US
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="../js//script.js"></script><link rel="stylesheet" href="../css/site.css">
+
+<div class="footer-content">
+    <div class="box-container">
+        <div class="footer-grid">
+            <div class="fitem1">
+                <div class="title">
+                    <span class="logo">
+                    <img src="https://avatars.githubusercontent.com/u/111488170?s=200&v=4" alt="" class="footer-logo"></span>
+                    <div class="title">
+                        JKCSS - CSS Framework
+                    </div>
+                    <div class="body">
+                        JKCSS is the CSS Framework. and you can use this Framework for design your website faster and responsive.
+                        <br></br>
+                        JKCSS, licensed <a href="https://github.com/JKCSS/JKCSS-Framework/blob/main/LICENSE" target="_blank" style="text-decoration: none; color:rgb(0, 88, 204); font-weight: 600;">MIT</a>
+                        <br><br><br>
+                        Currently v1
+                    </div>
+                </div>
+            </div>
+            <div class="fitem2">
+                <div class="title">
+                    Links
+                </div>
+            </div>
+            <div class="fitem3">
+                <div class="title">
+                    Projects
+                </div>
+            </div>
+            <div class="fitem4">
+                <div class="title">
+                    Contact US
+                </div>
+            </div>
         </div>
     </div>
 </div>
