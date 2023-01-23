1df17b73ea88d5d2e58e970948cf7329669e8823
JehanKandy <jehankandy@gmail.com>
2023-01-23 08:03:09 +0530

    Update header.php

--- layouts/header.php
+++ layouts/header.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JKCSS</title>
-    <link rel="icon" href="https://avatars.githubusercontent.com/u/111488170?s=200&v=4">    
+    <link rel="icon" href="https://avatars.githubusercontent.com/u/111488170?s=200&v=4">   
+    <script src="https://kit.fontawesome.com/c5846604ac.js" crossorigin="anonymous"></script>
 
 </head>
 <body>
