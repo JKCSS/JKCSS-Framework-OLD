c39051014863bdc0efc5f75b24f597a8da9cff07
JehanKandy <jehankandy@gmail.com>
2023-01-23 07:38:46 +0530

    Update footer_main.php

--- layouts/footer_main.php
+++ layouts/footer_main.php
@@ -1,3 +1,34 @@
 <link rel="stylesheet" href="../css/site.css">
 
+<footer>
+    <div class="footer-content">
+        <div class="box-container">
+            <div class="footer-grid">
+                <div class="fitem1">
+                    <div class="title">JKCSS</div>
+                    <div class="body">
+                        Develop user own Websites with JKCSS Faster than Faster
+                    </div>
+                </div>
+                <div class="fitem2">
+                    <div class="title">
+                        Links
+                    </div>
+                    <div class="boy">
+                        <ul>
+                            <li><a href="">Get Started</a></li>
+                            <li><a href="">Documentation</a></li>
+                            <li><a href="">Link 1</a></li>
+                            <li><a href="">Link 2</a></li>
+                            <li><a href="">Link 3</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="fitem3"></div>
+                <div class="fitem4"></div>
+            </div>
+        </div>
+    </div>
+</footer>
+
 <script src="../js/script.js"></script>
