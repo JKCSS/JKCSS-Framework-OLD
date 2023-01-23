a5d08555178a23e5c6607f4f9b37714712e9dd88
JehanKandy <jehankandy@gmail.com>
2023-01-23 06:55:22 +0530

    Update nav_main.php

--- layouts/nav_main.php
+++ layouts/nav_main.php
@@ -1,3 +1,11 @@
 <link rel="stylesheet" href="style.css">
 
+<nav>
+    <div class="main-nav">
+        <div class="box-container">
+            hi
+        </div>
+    </div>
+</nav>
+
 <script src="../js/script.js"></script>
