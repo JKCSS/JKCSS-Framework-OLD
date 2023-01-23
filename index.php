62859120dba621823fdc067a7372e92c73173bb2
JehanKandy <jehankandy@gmail.com>
2023-01-23 20:01:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,11 @@
 <?php include("layouts/header.php"); ?>
 <?php include("layouts/nav_main.php"); ?>
 
+<div class="main-content">
+    <div class="box-container">
+        hi
+    </div>
+</div>
+
 <?php include("layouts/footer_main.php"); ?>
 <script src="js/script.js"></script>
