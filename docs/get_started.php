5a41117661a02f8757a5221c340f8d453f84216d
JehanKandy <jehankandy@gmail.com>
2023-01-26 08:18:38 +0530

    Add files via upload

---
+++ docs/get_started.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="../css/site.css">
+<link rel="icon" href="https://avatars.githubusercontent.com/u/111488170?s=200&v=4">   
+<?php include("../layouts/nav_main.php"); ?>
+<?php include("../layouts/header.php"); ?>
+
+<section class="get-ses1">
+    <div class="box-container">
+        <div class="title">
+            Introduction
+        </div>
+        <div class="body">
+
+        </div>
+    </div>
+</section>
+
+<?php include("../layouts/page_footer.php"); ?>
+<script src="../js/script.js"></script>
