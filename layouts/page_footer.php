26472b626cee86fe786a09d55520119c4c883f2f
JehanKandy <jehankandy@gmail.com>
2023-01-26 07:46:44 +0530

    Update page_footer.php

--- layouts/page_footer.php
+++ layouts/page_footer.php
@@ -1,5 +1,13 @@
 <link rel="stylesheet" href="../css/site.css">
-
+<footer>
+    <div class="page-footer">
+        <div class="container">
+            <div class="copy">
+                <div class="copy">All Rights Reserved By <b><i class="far fa-copyright"></i><a href="https://github.com/JehanKandy" target="_blank">JehanKandy</a></b> 2021 - 2023, <a href="https://github.com/JKCSS/JKCSS-Framework" target="_blank">JKCSS</a></div>
+            </div>
+        </div>
+    </div>
+</footer>
 
 
 <script src="../js/script.js"></script>
